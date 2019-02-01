0e9c292934350732af49d717a0f194bc890d630a
Andres Monterrosa <felipe.monterrosa@hotmail.com>
2019-02-01 16:43:33 -0500

    Curso AngularJS

---
+++ miAplicacionAngular2/app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name miAplicacionAngular2App
+ * @description
+ * # miAplicacionAngular2App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('miAplicacionAngular2App', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ miAplicacionAngular2/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name miAplicacionAngular2App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the miAplicacionAngular2App
+ */
+angular.module('miAplicacionAngular2App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
